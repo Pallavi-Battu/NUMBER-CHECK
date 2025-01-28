4688547bec40c19a2f20fa5264a08fa8b9ac1100
Pallavi-Battu <pallavibattu5@gmail.com>
2025-01-28 18:06:54 +0530

    Add files via upload

---
+++ numbercheck.java
@@ -0,0 +1,15 @@
+public class numbercheck {
+    public static void main(String[] args) {
+        int number=5;
+        if(number>0){
+            System.out.println("the number is positive");
+        }else if(number<0){
+            System.out.println("the number is negative");
+        }else{
+            System.out.println("the number is zero");
+        }
+        
+
+        }
+    
+}
